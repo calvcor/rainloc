0ba38d31367ba2eae7ca7971a09941f32b855268
Carlos Alventosa <calvcor@teleco.upv.es>
2026-09-20 13:28:01 +0200

    feat(mobile): convertir barra inferior en una píldora flotante integrada sobre el mapa

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Estrategia Network-First con Auto-Update para evitar atascos de caché
  */
 
-const CACHE_NAME = 'rainloc-pwa-v3.8';
+const CACHE_NAME = 'rainloc-pwa-v3.9';
 
 // Recursos críticos para el funcionamiento offline básico
 const PRECACHE_ASSETS = [
